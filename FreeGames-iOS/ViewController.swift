3291fef143c5c58e6f19d860a4205700a89b83af
IgniteCoders <theboom.kajas@gmail.com>
2025-11-12 12:17:56 +0100

    Initial Commit

---
+++ FreeGames-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FreeGames-iOS
+//
+//  Created by Mananas on 12/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
